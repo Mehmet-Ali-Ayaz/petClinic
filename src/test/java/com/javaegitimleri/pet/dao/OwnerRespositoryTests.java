d5801eb4077fb245813168d2a5bbc7f14ea3e3e2
Mehmet Ali Ayaz <73446446+Mehmet-Ali-Ayaz@users.noreply.github.com>
2020-12-20 19:57:31 +0300

    OwnerRepositoryTests sinifi olusturuldu schema sqlde değisiklik yapildi

---
+++ src/test/java/com/javaegitimleri/pet/dao/OwnerRespositoryTests.java
@@ -0,0 +1,44 @@
+package com.javaegitimleri.pet.dao;
+
+
+import com.javaegitimleri.pet.model.Owner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.swing.text.html.parser.Entity;
+import javax.transaction.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+@Transactional
+public class OwnerRespositoryTests {
+
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test(expected = PersistenceException.class)
+    public void testCreateOwner(){
+        Owner owner=new Owner();
+        owner.setFirstName(null);
+        owner.setLastName(null);
+
+        ownerRepository.create(owner);
+
+        entityManager.flush();
+
+
+    }
+
+
+}
